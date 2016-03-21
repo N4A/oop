f0a174e324a78dfca7bf4e252365ad333fde9ded
N4A <181387406@qq.com>
2016-03-21 19:33:17 +0800

    lab1:problem1:pointer and refferrence

---
+++ lab1/lab1_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+     int a = 1;
+     int *ap = &a;
+     int &ar = a;
+     cout << &a << endl;
+     cout << &ap << endl;
+     cout << &ar << endl;
+     return 0;
+}
